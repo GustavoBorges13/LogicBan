6f2e6c7f7a3c19622efdc2f1f47d88e09b32bf6e
Gustavo Borges <ggustavo.borges13@gmail.com>
2025-03-18 18:40:31 -0300

    Update Player.java

--- src/main/java/br/ufcat/logicban/entity/Player.java
+++ src/main/java/br/ufcat/logicban/entity/Player.java
@@ -169,7 +169,7 @@ public class Player extends Entity {
 
 				// CHECK TILE COLLISION
 				collisionOn = false;
-				// gp.cChecker.checkTile(this);
+				gp.cChecker.checkTile(this);
 
 				// CHECK OBJECT COLLISION
 				int objIndex = gp.cChecker.checkObject(this, true);
